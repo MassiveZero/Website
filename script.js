cf7c0f62e5c5b99bdd4f11d9a430d54b7b275461
MassiveZero <wksorley@gmail.com>
2025-11-30 13:30:28 +0000

    Feat: add animation on nav bar and finalise stuff

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ const toggle = document.getElementById('darkModeToggle');
 toggle.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
 });
+
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+    const header = document.getElementById("header");
+    const current = window.scrollY;
+
+    if (current > lastScroll + 5) {
+        header.style.transform = "translateY(-100%)";
+    } else if (current < lastScroll - 5) {
+        header.style.transform = "translateY(0)";
+    }
+
+    lastScroll = current;
+});
